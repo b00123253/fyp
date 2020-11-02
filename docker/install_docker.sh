c241fde02029857ba0aab8e54b9e5bad02e3b37e
b00123253 <73838081+b00123253@users.noreply.github.com>
2020-11-02 23:06:32 +0000

    Update install_docker.sh

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -1,4 +1,4 @@
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y docker-ce docker-ce-cli containerd.io
